da4e793929b2c9cc43d8089b492dea7ca250ceee
Muhamad Sumair Shaukat <sumairch224@gmail.com>
2024-01-06 18:13:11 +0500

    added error-middleware and setup server

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const createError = require("http-errors");
+
+require("dotenv").config();
+const app = express();
+
+app.get("/", async (req, res) => {
+  res.send("hello from the server");
+});
+
+app.use(async (req, res, next) => {
+  next(createError.NotFound());
+});
+
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App is Listening on port ${port}`);
+});
